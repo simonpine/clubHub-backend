1291299490eb8ea204fefd0220ff9cf22e3fef23
simon pineda <simonpineda0521@gmail.com>
2023-06-29 23:40:55 -0500

    now when the user get into a club the user img do not reolad, the background no longer uses blur filter, the name filter in grades is working, and socket io es working on events section

--- src/index.js
+++ src/index.js
@@ -2,18 +2,40 @@
 import { PORT } from "./config";
 import { server } from './app'
 import { Server as SocketServer } from 'socket.io'
+import { connect } from "./database"
 
 const httpServer = server.listen(PORT)
 
 const io = new SocketServer(httpServer, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: 'https://simonpine.com/',
     }
 })
 
 
 io.on('connection', (socket) => {
-    socket.join(socket.handshake.query.myParam)
+    // socket.removeAllListeners()
+
+    console.log('===================conn')
+
+    socket.on('joinClub', evt => {
+        socket.join(evt)
+    })
+    socket.on('newEventMessage', mess => {
+
+        io.to(mess).emit('emitMessage', mess)
+    })
+    // socket.join('asdasd')
+
+    // console.log(socket.rooms)
+    // socket.on("disconnect", () => {
+    //     console.log(socket.connected); // false
+    // })
+
+})
+
+io.on('disconnection', (socket) => {
+    console.log('desconectado');
 })
 
 console.log('Server on port ' + PORT)
