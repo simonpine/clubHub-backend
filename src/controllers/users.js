80882a92cedb10dae8812c221c2f889e966ba9c3
Simon Pineda <76760387+simonpine@users.noreply.github.com>
2024-01-31 13:38:34 -0500

    Update users.js

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -10,13 +10,31 @@ export const getUsers = async (req, res) => {
     clubs JSON NOT NULL,
     pasword VARCHAR(45) NOT NULL,
     userImg VARCHAR(255) DEFAULT NULL,
-    question VARCHAR(45) NOT NULL,
+    question TEXT NOT NULL,
     answer VARCHAR(45) NOT NULL,
     description TEXT NOT NULL,
     PRIMARY KEY (userName)
 );
-
 `)
+    await connection.query(`
+    CREATE TABLE IF NOT EXISTS clubs (
+	id VARCHAR(45) NOT NULL,
+    title VARCHAR(45) NOT NULL,
+    description TEXT NOT NULL,
+    gardes JSON NOT NULL,
+    clubBanner VARCHAR(255) NOT NULL,
+	members JSON DEFAULT NULL,
+	chat JSON DEFAULT NULL,
+	events JSON DEFAULT NULL,
+    clubOwner VARCHAR(45) NOT NULL,
+	clubLeader VARCHAR(45) NOT NULL,
+	existGrades BOOLEAN NOT NULL,
+	existChat BOOLEAN NOT NULL,
+    calendarEvents JSON DEFAULT NULL, 
+	surveys JSON DEFAULT NULL, 
+    PRIMARY KEY (id)
+);
+    `)
 
     res.json(rows)
 }
