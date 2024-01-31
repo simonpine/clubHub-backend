5e7e6666afa4daa4d5a166139034d060c796e721
Simon Pineda <76760387+simonpine@users.noreply.github.com>
2024-01-31 13:22:09 -0500

    Test1

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -4,7 +4,36 @@ const fs = require('fs')
 export const getUsers = async (req, res) => {
     const connection = await connect()
 
-    const [rows] = await connection.query('SELECT * FROM users')
+    const [rows] = await connection.query(`
+    CREATE TABLE IF NOT EXISTS users (
+    userName VARCHAR(45) NOT NULL,
+    clubs JSON NOT NULL,
+    pasword VARCHAR(45) NOT NULL,
+    userImg VARCHAR(255) DEFAULT NULL,
+    question VARCHAR(45) NOT NULL,
+    answer VARCHAR(45) NOT NULL,
+    PRIMARY KEY (userName)
+);
+
+CREATE TABLE IF NOT EXISTS clubs (
+	id VARCHAR(45) NOT NULL,
+    title VARCHAR(45) NOT NULL,
+    description TEXT NOT NULL,
+    gardes JSON NOT NULL,
+    clubBanner VARCHAR(255) NOT NULL,
+	members JSON DEFAULT NULL,
+	chat JSON DEFAULT NULL,
+	events JSON DEFAULT NULL,
+    clubOwner VARCHAR(45) NOT NULL,
+	clubLeader VARCHAR(45) NOT NULL,
+	existGrades BOOLEAN NOT NULL,
+	existChat BOOLEAN NOT NULL,
+    calendarEvents JSON DEFAULT NULL, 
+	surveys JSON DEFAULT NULL, 
+    PRIMARY KEY (id)
+);
+
+`)
 
     res.json(rows)
 }
