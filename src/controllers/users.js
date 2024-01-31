7bf9e0894aa7b78e978f2eec26e0544f54cf5edd
Simon Pineda <76760387+simonpine@users.noreply.github.com>
2024-01-31 13:32:24 -0500

    lasttest

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -12,25 +12,8 @@ export const getUsers = async (req, res) => {
     userImg VARCHAR(255) DEFAULT NULL,
     question VARCHAR(45) NOT NULL,
     answer VARCHAR(45) NOT NULL,
-    PRIMARY KEY (userName)
-);
-
-CREATE TABLE IF NOT EXISTS clubs (
-    id VARCHAR(45) NOT NULL,
-    title VARCHAR(45) NOT NULL,
     description TEXT NOT NULL,
-    gardes JSON NOT NULL,
-    clubBanner VARCHAR(255) NOT NULL,
-    members JSON DEFAULT NULL,
-    chat JSON DEFAULT NULL,
-    events JSON DEFAULT NULL,
-    clubOwner VARCHAR(45) NOT NULL,
-    clubLeader VARCHAR(45) NOT NULL,
-    existGrades BOOLEAN NOT NULL,
-    existChat BOOLEAN NOT NULL,
-    calendarEvents JSON DEFAULT NULL, 
-    surveys JSON DEFAULT NULL, 
-    PRIMARY KEY (id)
+    PRIMARY KEY (userName)
 );
 
 `)
